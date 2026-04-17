a0f84e1714f5b928cb6e7508a3fca19df5ee55a6
Amr Kahla <amrkahla6@gmail.com>
2026-04-17 16:25:16 +0200

    array - sum code php using HashMap / Associative Array

--- array/PHP/twoSum/two-sum.php
+++ array/PHP/twoSum/two-sum.php
@@ -3,16 +3,17 @@
     $array = [2,7,11,15];
     $target = 9;
 
-    print_r(sum($array,$target));
+    print_r(twoSum($array,$target));
 
-    function sum($nums,$target)
+    function twoSum($nums,$target)
     {
-        for ($i=0; $i < count($nums); $i++) {
-            for ($j= $i+1; $j < count($nums); $j++) {
-                if($nums[$i] + $nums[$j] == $target) {
-                    return [$i, $j];
-                }
+        $map = [];
+        foreach ($nums as $i => $num) {
+            $complement = $target - $num;
+            if (isset($map[$complement])) {
+                return [$map[$complement], $i];
             }
+            $map[$num] = $i;
         }
         return [];
     }
