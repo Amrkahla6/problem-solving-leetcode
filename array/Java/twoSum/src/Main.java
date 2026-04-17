79935eb578ad565bdfc086e7d6b6dffcbc5d753e
Amr Kahla <amrkahla6@gmail.com>
2026-04-17 15:59:36 +0200

    array - sum code using hashmap

--- array/Java/twoSum/src/Main.java
+++ array/Java/twoSum/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,14 +12,17 @@ public class Main {
     }
 
     public static int[] twoSum(int[] nums, int target){
-        for (int i =0; i < nums.length; i++){
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
-            }
-        }
+        HashMap<Integer,Integer> map = new HashMap<>();
 
+        for (int i = 0; i < nums.length; i ++){
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+
+            map.put(nums[i],i);
+        }
         return new int[]{};
     }
 }
