393fc91ab1a0402f474594e6fa97c438b4c33977
Amr Kahla <amrkahla6@gmail.com>
2026-04-17 15:51:52 +0200

    array - sum code first solution

---
+++ array/Java/twoSum/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        int [] arr = {2,7,11,15};
+        int target = 9;
+       System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target){
+        for (int i =0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
