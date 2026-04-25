04c3aaa27f69cc2caa56f40f9e80000dee320c38
Amr Kahla <amrkahla6@gmail.com>
2026-04-25 14:47:18 +0300

    array - Contains Duplicate using PHP (Hashmap)

--- array/PHP/5-containsDuplicate/two-sum.php
+++ array/PHP/5-containsDuplicate/two-sum.php
@@ -1,19 +1,20 @@
 <?php
 
-$nums = [1,2,3,4];
+$nums = [1,2,3,1];
 
     print_r(containsDuplicate($nums));
 
     function containsDuplicate($nums)
     {
-        sort($nums);
-        for ($i = 0; $i < count($nums); $i++) {
-            if ($nums[$i] == $nums[$i + 1]) {
-                return "true";
-            }
-        }
+       $set = [];
+       foreach ($nums as $num) {
+           if (isset($set[$num])) {
+               return "true";
+           }
+           $set[$num] = true;
+       }
 
-        return "false";
+       return "false";
     }
 
 
