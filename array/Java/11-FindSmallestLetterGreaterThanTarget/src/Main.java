92f4cc135e9a20094a969292d7c61475f222c9fe
Amr Kahla <amrkahla6@gmail.com>
2026-06-20 11:39:24 +0300

    Find Smallest Letter Greater Than Target using java

---
+++ array/Java/11-FindSmallestLetterGreaterThanTarget/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    public static void main(String[] args){
+        char[] letters = {'c','f','j'};
+        char target = 'c';
+        System.out.println(nextGreatestLetter(letters,target));
+    }
+
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        for(int i = 0; i < letters.length; i++){
+            if (letters[i] > target){
+                return letters[i];
+            }else if (i == letters.length - 1){
+                return letters[0];
+            }
+        }
+
+        return target;
+    }
+}
