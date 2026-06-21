abd23182df0d121ba0c8230ba3c3f5ee4964fa21
Amr Kahla <amrkahla6@gmail.com>
2026-06-21 11:52:41 +0300

    solve search problem using Binary search java

--- binarySearch/Java/BinarySearch/src/Main.java
+++ binarySearch/Java/BinarySearch/src/Main.java
@@ -13,10 +13,22 @@ class Main {
 
 
     public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                return i;
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            if (nums[mid] > target) {
+                right = mid - 1;
             }
         }
 
