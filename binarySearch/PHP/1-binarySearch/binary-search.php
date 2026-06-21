16d3d044cbc7982b2e2a55fdfcb55ac9dbb2b37f
Amr Kahla <amrkahla6@gmail.com>
2026-06-21 12:06:12 +0300

    solve search problem using Binary search PHP

--- binarySearch/PHP/1-binarySearch/binary-search.php
+++ binarySearch/PHP/1-binarySearch/binary-search.php
@@ -7,14 +7,26 @@
 
     function search($nums,$target)
     {
-        $countNum = count($nums);
-        for ($i = 0; $i < $countNum; $i++){
-            if ($nums[$i] == $target){
-                return $i;
-            }
-        }
+      $left = 0;
+      $right = count($nums) - 1;
 
-        return -1;
+      while ($left <= $right){
+          $mid = floor($left + $right) / 2;
+
+          if ($nums[$mid] == $target){
+              return $mid;
+          }
+
+          if ( $nums[$mid] < $target){
+              $left = $mid + 1;
+          }
+
+          if ($nums[$mid] > $target){
+              $right = $mid - 1;
+          }
+      }
+
+      return  -1;
     }
 
 
