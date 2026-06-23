434479784c5b203df1bc12847d29be43d02998fe
Amr Kahla <amrkahla6@gmail.com>
2026-06-23 12:09:11 +0300

    Is Bad version uisng java

---
+++ binarySearch/Java/3-FirstBadVersion/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(firstBadVersion(n));
+    }
+
+    public static int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start < end){
+            int mid = start + (end - start)/2;
+
+            if (isBadVersion(mid)){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+}
